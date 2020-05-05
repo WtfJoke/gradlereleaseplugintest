e9fe81815c4cc136d25da08d1252f31682ed2b52
Manuel <manuel.wessner@codecentric.de>
2020-05-05 11:56:56 +0200

    Add gradle release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
 	kotlin("jvm") version "1.3.71"
 	kotlin("plugin.spring") version "1.3.71"
+	id("net.researchgate.release") version "2.6.0"
 }
 
 group = "com.example"
